ce5ec6cd0bbb83b8c5b22e5bdd81bc0c0e23aa3e
David <xudavidb@gmail.com>
2016-05-07 22:09:49 -0700

    test list

--- app/entry.js
+++ app/entry.js
@@ -1,2 +1,21 @@
+import List from './components/List'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 let x = 10
 console.log(x)
+
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(() => {
+  ReactDOM.render(
+    <List />,
+    document.body
+  )
+})
