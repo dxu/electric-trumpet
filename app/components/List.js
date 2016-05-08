3f3075f0b532ce5693763b9ff43d9e4fcb713a69
David <xudavidb@gmail.com>
2016-05-07 22:09:30 -0700

    add list component

---
+++ app/components/List.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const List = React.createClass({
+  render: () => {
+    return (
+      <div>
+        Hello World. THIS IS AMAZING
+      </div>
+    )
+  }
+})
+
+export default List
