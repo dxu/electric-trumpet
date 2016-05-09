33ab5dbebaf8574352c7ed599023f822c0e2455a
David <xudavidb@gmail.com>
2016-05-08 17:47:51 -0700

    test store

--- app/entry.js
+++ app/entry.js
@@ -1,6 +1,10 @@
 import App from './components/App'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Immutable from 'immutable'
+
+import { addItem, addArchive } from './actions'
+import store from './store'
 
 let x = 10
 console.log(x)
@@ -18,4 +22,39 @@ ready(() => {
     <App />,
     document.getElementById('genesis')
   )
+
+
+  // Log the initial state
+  console.log(store.getState())
+
+  // Every time the state changes, log it
+  // Note that subscribe() returns a function for unregistering the listener
+  let unsubscribe = store.subscribe(() =>
+    console.log(store.getState().toJS())
+  )
+
+  // Dispatch some actions
+  store.dispatch(addItem({
+    text: 'item name 1'
+  }))
+  store.dispatch(addItem({
+    text: 'item name 2'
+  }))
+  store.dispatch(addItem({
+    text: 'item name 3'
+  }))
+  store.dispatch(addArchive({
+    name: 'archive name 1'
+  }))
+  store.dispatch(addArchive({
+    name: 'archive name 2'
+  }))
+  store.dispatch(addArchive({
+    name: 'archive name 3'
+  }))
+
+  // Stop listening to state updates
+  unsubscribe()
+
+
 })
