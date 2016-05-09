22dff662ee3cb998580a38376f15ae1b13e76046
David <xudavidb@gmail.com>
2016-05-08 23:54:11 -0700

    add default uncategorized category to default store

--- app/store.js
+++ app/store.js
@@ -2,4 +2,16 @@ import { createStore } from 'redux'
 import reducers from './reducers'
 import Immutable from 'immutable'
 
-export default createStore(reducers, Immutable.Map())
+import { DEFAULT_ARCHIVE_ID, DEFAULT_ARCHIVE_NAME } from './util/constants'
+
+const defaultStore = {
+  archives: [
+    {
+      id: DEFAULT_ARCHIVE_ID,
+      name: DEFAULT_ARCHIVE_NAME
+    }
+  ],
+  activeArchive: DEFAULT_ARCHIVE_ID
+}
+
+export default createStore(reducers, Immutable.fromJS(defaultStore))
