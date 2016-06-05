21f1b044b6def2de1e8b19252139c03dbe25aba0
David <xudavidb@gmail.com>
2016-06-04 17:09:04 -0700

    add toggle for quicknote

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const electron = require('electron');
-const app = electron.app;  // Module to control application life.
-const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+const {app, globalShortcut, BrowserWindow} = require('electron')
+const channels = require('./app/util/channels')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
-var quickNote = null;
+var mainWindow = null
+var quickNote = null
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
@@ -21,9 +20,10 @@ app.on('window-all-closed', function() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
+
   // Create the browser window.
   mainWindow = new BrowserWindow({ width: 600, height: 800, frame: false });
-  quickNote = new BrowserWindow({ width: 600, height: 300, frame: false });
+  quickNote = new BrowserWindow({ width: 600, height: 300, frame: false, show: false });
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
@@ -33,11 +33,38 @@ app.on('ready', function() {
   mainWindow.webContents.openDevTools();
   quickNote.webContents.openDevTools();
 
+  // Register a 'CommandOrControl+X' shortcut listener.
+  const ret = globalShortcut.register('CommandOrControl+Shift+K', () => {
+    console.log('CommandOrControl+K is pressed');
+    if (quickNote.isVisible()) {
+      quickNote.hide()
+    } else {
+      quickNote.show()
+    }
+
+    
+  });
+
+  if (!ret) {
+    console.log('registration failed');
+  }
+
+  // Check whether a shortcut is registered.
+  console.log(globalShortcut.isRegistered('CommandOrControl+X'));
+
+
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
+  });
+
+  // Emitted when the window is closed.
+  quickNote.on('close', function(evt) {
+    // never close the quickNote, only hide it
+    evt.preventDefault()
+    quickNote.hide()
   });
 });
